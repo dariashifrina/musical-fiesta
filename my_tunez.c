81d006686ec898a550bae222f07a443476ae720b
Inbar Pe'er <inbarpeer@Inbars-MacBook-Air.local>
2017-10-19 11:42:09 -0400

    worked

--- my_tunez.c
+++ my_tunez.c
@@ -37,15 +37,16 @@ struct song_node * insert_front(struct song_node *p, char n[256], char a[256]){
   return newbie;
 }
 //inserts node at the front
-struct song_node * insert(struct song_node *p, char n[], char a[]){
+struct song_node * insert(struct song_node *p, char n[256], char a[256]){
+  struct song_node*front = p;
   while(p){
-    if(strcmp(p->next->name,n)<0){
+    if(strcmp(p->name,n)<0){
      struct song_node *newbie= malloc(sizeof(struct song_node));
+     newbie->next = p->next;
      strcpy(newbie->name,n);
      strcpy(newbie->artist,a);
-     newbie->next = p->next;
      p->next = newbie;
-     return newbie;
+     return front ;
     }
     p=p->next;
   }
@@ -81,6 +82,8 @@ void remove_song(struct song_node * song){
 int main(){
   struct song_node *yay = 0; 
   yay= insert_front(yay, "How Far I'll Go", "Auli'i Cravalho");
+  yay= insert(yay, "Single Ladies", "Beyonce" );
+  yay = insert(yay, "Countdown", "Beyonce");
   print_list(yay);
   return 0;
 }
