6a524344557072e8652b360f6f2c94ffd5a8da83
Inbar Pe'er <inbarpeer@Inbars-MacBook-Air.local>
2017-10-20 00:07:19 -0400

    eep

--- linked_list.c
+++ linked_list.c
@@ -23,12 +23,92 @@ void delete_song(char song[]);
 void delete_all();
 
 int letter_converter(char* a){
-  return (int)(a - 65);
+  if(a = 'A'){
+    return 1;
+  }
+  if(a = 'B'){
+    return 2;
+  }
+  if(a = 'C'){
+    return 3;
+  }
+  if(a = 'D'){
+    return 4;
+  }
+  if(a = 'E'){
+    return 5;
+  }
+   if(a = 'F'){
+    return 6;
+  }
+  if(a = 'G'){
+    return 7;
+  }
+  if(a = 'H'){
+    return 8;
+  }
+  if(a = 'I'){
+    return 9;
+  }
+  if(a = 'J'){
+    return 10;
+  }
+   if(a = 'K'){
+    return 11;
+  }
+  if(a = 'L'){
+    return 12;
+  }
+  if(a = 'M'){
+    return 13;
+  }
+  if(a = 'N'){
+    return 14;
+  }
+  if(a = 'O'){
+    return 15;
+  }
+   if(a = 'P'){
+    return 16;
+  }
+  if(a = 'Q'){
+    return 17;
+  }
+  if(a = 'R'){
+    return 18;
+  }
+  if(a = 'S'){
+    return 19;
+  }
+  if(a = 'T'){
+    return 20;
+  }
+   if(a = 'U'){
+    return 21;
+  }
+  if(a = 'V'){
+    return 22;
+  }
+  if(a = 'W'){
+    return 23;
+  }
+  if(a = 'X'){
+    return 24;
+  }
+  if(a = 'Y'){
+    return 25;
+  }
+   if(a = 'Z'){
+    return 26;
+  }
 }
 
-//void add_song(struct song_node * song){
-// int place = (int)(
-// }
+void add_song(struct song_node * song){
+  int place = letter_converter(song-->song[0]);
+  
+    
+    
+    } 
 struct song_node * find_songz(struct song_node * songnode, char song[], char artist[]){
   return 0;
 }
