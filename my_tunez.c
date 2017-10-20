ae2ef9edc48f2376d416786de1763c3b0950518e
Inbar Pe'er <inbarpeer@Inbars-MacBook-Air.local>
2017-10-19 20:34:04 -0400

    stuff

--- my_tunez.c
+++ my_tunez.c
@@ -37,22 +37,30 @@ struct song_node * insert_front(struct song_node *p, char n[256], char a[256]){
   return newbie;
 }
 //inserts node at the front
+
+
 struct song_node * insert(struct song_node *p, char n[256], char a[256]){
   struct song_node*front = p;
   while(p){
-    if(strcmp(p->name,n)<0){
-     struct song_node *newbie= malloc(sizeof(struct song_node));
-     newbie->next = p->next;
-     strcpy(newbie->name,n);
-     strcpy(newbie->artist,a);
-     p->next = newbie;
-     return front ;
+    if(strcmp(p->artist,a)<0){
+      if(strcmp(p->name, n)<0){
+	insert_help(p, n, a);
+      }
     }
     p=p->next;
   }
-  return 0;
+  insert_help(p,n,a);
+  return front;
+  
 }
-
+void insert_help(struct song_node *p, char n[256], char a[256]){
+struct song_node *newbie= malloc(sizeof(struct song_node));
+	newbie->next = p->next;
+	strcpy(newbie->name,n);
+	strcpy(newbie->artist,a);
+	p->next = newbie;
+}
+// inserts node alphabetically
 struct song_node * free_list(struct song_node *p){
   struct song_node *f = p;
   while(p->next){
@@ -79,12 +87,14 @@ struct song_node * random_song(struct song_node * song){
 void remove_song(struct song_node * song){
 }
 
-int main(){
-  struct song_node *yay = 0; 
+int main(int argc, char *argv[]){
+  struct song_node *yay = 0;
+  printf("starting\n");
   yay= insert_front(yay, "How Far I'll Go", "Auli'i Cravalho");
   yay= insert(yay, "Single Ladies", "Beyonce" );
   yay = insert(yay, "Countdown", "Beyonce");
   print_list(yay);
+  printf("finishing\n");
   return 0;
 }
 
