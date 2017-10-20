615e1ee93a8a422bab0b48b1b907bd3887285d49
Inbar Pe'er <inbarpeer@Inbars-MacBook-Air.local>
2017-10-19 23:30:41 -0400

    main

--- my_tunez.c
+++ my_tunez.c
@@ -175,22 +175,3 @@ void remove_song(struct song_node * p, char song[],char artist[]){
   
 }
 
-int main(int argc, char *argv[]){
-  struct song_node *yay = 0;
-  printf("starting\n");
-  yay=insert_front(yay, "woo", "hoo");
-  yay= insert_front(yay, "How Far I'll Go", "Auli'i Cravalho");
-  yay= insert(yay, "Single Ladies", "Beyonce" );
-  yay = insert(yay, "Countdown", "Beyonce");
-  print_list(yay);
-  printf("testing to see if single ladies can be found: %s\n", find_song(yay, "Single Ladies", "Beyonce"));
-  printf("testing to see if beyonce can be found: %s\n", find_artist(yay,"Beyonce"));
-  //printf("%d", list_size(yay));
-  printf("testing random song pointer: %s\n", random_song(yay));
-  printf("testing remove song: \n");
-  remove_song(yay, "Single Ladies", "Beyonce");
-  print_list(yay);
-  printf("finishing\n");
-  
-  return 0;
-}
