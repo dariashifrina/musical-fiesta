8fd95f4916d7b4cb396c2895d4562d1875318cc4
dariashifrina <dshifrina@stuy.edu>
2017-10-19 23:28:25 -0400

    linked list file

---
+++ linked_list.c
@@ -0,0 +1,23 @@
+struct song_node * library[26];
+void add_song(struct song_node *);
+struct song_node * find_song(struct song_node *, char song[], char artist[]);
+struct song_node * find_artist(struct song_node *, char artist[]);
+void print_song(char a);
+void print_artist(char artist[]);
+void print_library();
+void shuffle();
+void delete_song(song[]);
+void delete_all();
+
+/*void add_song(struct song_node * song){
+  int place = (int)(
+  }*/
+
+struct song_node * find_song(struct song_node * song, char song[], char artist[]){
+  return 0;
+}
+
+int main(){
+  return 0;
+}
+
